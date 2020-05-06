b16810a6d32f176cefafbb2a79712b30c7b8d3e1
JotaroYamamoto <kensin2131@gmail.com>
2020-05-06 23:39:42 +0900

    test途中経過

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,16 +3,17 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use App\User;
+
+
+
 class ExampleTest extends TestCase
 {
     public function testBasicTest()
     {
         $response=$this->get('/');
         $response->assertStatus(200);
+
     }
 
 }
